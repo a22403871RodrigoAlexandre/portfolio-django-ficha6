8399293d394adc4ddc4e7d8cc1f329eeba806794
a22403871RodrigoAlexandre <a22403871@alunos.ulht.pt>
2026-04-08 15:02:06 +0000

    Alteracao de script

--- load_tfcs.py
+++ load_tfcs.py
@@ -1,24 +1,24 @@
-import json
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+django.setup()
+
+import json
 from django.conf import settings
-from django.core.management.base import BaseCommand
 from portfolio.models import TFC
 
-class Command(BaseCommand):
-    help = 'Carrega TFCs do JSON'
+path = os.path.join(settings.BASE_DIR, 'data', 'tfcs_2025.json')
 
-    def handle(self, *args, **kwargs):
-        path = os.path.join(settings.BASE_DIR, 'data', 'tfcs_2025.json')
+with open(path, encoding='utf-8') as f:
+    dados = json.load(f)
 
-        with open(path, encoding='utf-8') as f:
-            dados = json.load(f)
+for item in dados:
+    TFC.objects.create(
+        titulo=item.get('titulo'),
+        autor=item.get('autor'),
+        ano=item.get('ano'),
+        descricao=item.get('descricao', ''),
+    )
 
-        for item in dados:
-            TFC.objects.create(
-                titulo=item.get('titulo'),
-                autor=item.get('autor'),
-                ano=item.get('ano'),
-                descricao=item.get('descricao', ''),
-            )
-
-        self.stdout.write(self.style.SUCCESS('Dados carregados com sucesso!'))
+print('Dados carregados com sucesso!')
