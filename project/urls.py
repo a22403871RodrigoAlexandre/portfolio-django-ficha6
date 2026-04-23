889ddb2bf22c0e0e2c08ba705da602523064830e
a22403871RodrigoAlexandre <a22403871@alunos.ulht.pt>
2026-04-23 21:07:21 +0000

    Introdução da App Escola

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include              #     <- adicionar include 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("escola/", include("escola.urls")), 
+    path("", include("escola.urls")),  #  rota para app escola sem precisar de escrever "escola"
 ]
 
+# Serve ficheiros media em DEBUG
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
